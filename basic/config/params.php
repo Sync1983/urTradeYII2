304a6cf714adac891c726c9e6eb13498a4be94ed
Sync1983 <sync06@mail.ru>
2015-01-08 00:31:38 +0300

    Update params.php

--- basic/config/params.php
+++ basic/config/params.php
@@ -5,17 +5,5 @@ return [
     'providerUse' =>[
       app\models\search\ProviderOnline::class,
     ],
-    'providers'  => [
-      app\models\search\ProviderOnline::class=> [
-            'login'     => '6957659777',
-            'password'  => 'kdV2N5iD5w'
-      ],
-      /*  [ 'name'  =>  'Ixora',
-          'default_params' => [
-            'login'=>'AVTOTEHS',
-            'pass'=>'6de6b09l',
-            'contract_id'=>'86951'
-        ]
-      ]*/
     ]      
 ];
