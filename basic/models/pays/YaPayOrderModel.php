0aae6e485ebd7e6c343e93fc01264cfc577f0c71
Sync1983 <1983sync@gmail.com>
2015-05-17 23:04:21 +0400

    fix sum validator

--- basic/models/pays/YaPayOrderModel.php
+++ basic/models/pays/YaPayOrderModel.php
@@ -180,9 +180,9 @@ class YaPayOrderModel extends Model{
       $this->addError($attribute, "Ошибочное значение $attribute");
       return false;
     }
-
-    $price = $user->getUserPrice($record->price * $record->sell_count) * 1.0;
-    $new_price = $user->getUserPrice($record->pay_value + $this->$attribute) * 1.0;
+    $price = $user->getUserPrice($record->price) * $record->sell_count * 1.0;
+    $new_price = ($record->pay_value + $this->$attribute) * 0.97;
+    \yii::info("YA CheckOrder Price [$price] incomming [$new_price] (-3%) ", 'balance');
     if ( ($new_price - $price) > 1.0 ){
       $this->addError($attribute, "Переплата");
       return false;
