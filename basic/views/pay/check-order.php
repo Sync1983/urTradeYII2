33e6cfe60a8b2c8421d6d32e553eaf4f6b5b2bb4
Sync1983 <1983sync@gmail.com>
2015-05-18 05:23:15 +0400

    minor fix

--- basic/views/pay/check-order.php
+++ basic/views/pay/check-order.php
@@ -2,4 +2,4 @@
 /* @var $model app\models\pays\YaPayOrderModel */
 ?>
 <?= '<?xml version="1.0" encoding="UTF-8"?>'; ?>
-<checkOrderResponse performedDatetime = "<?= date("Y-m-d\TH:i:s.uP");?>" code = "<?= $code;?>" shopId = "<?= $model->shopId;?>" invoiceId	= "<?= $model->invoiceId;?>" orderSumAmount = "<?= $model->orderSumAmount;?>" <?php if ( $error ):?>message	= "<?= $error ?>"<?php endif;?>/>
+<checkOrderResponse performedDatetime = "<?= date("Y-m-d\TH:i:s.uP");?>" code = "<?= $code;?>" shopId = "<?= $model->shopId;?>" invoiceId	= "<?= $model->invoiceId;?>" orderSumAmount = "<?= $model->orderSumAmount;?>" <?php if ( $error ):?>message	= "<?= $error ?>"<?php endif;?>/> 
