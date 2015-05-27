dc5f552f13f215d2e6b25f0834c366e8c05cab72
Sync1983 <1983sync@gmail.com>
2015-05-27 21:38:03 +0400

    change news-block

--- basic/components/views/news_widget.php
+++ basic/components/views/news_widget.php
@@ -4,39 +4,36 @@ use app\models\news\NewsModel;
 use yii\web\View;
 /* @var $data NewsProvider */
 /* @var $this View */
-$this->registerJs("$('.carousel').carousel();");
+/* @var $row NewsModel */
+
+app\assets\NewsWidgetAsset::register($this);
+$this->registerJs("$(\".carousel-atc\").carousel_atc()");
 ?>
-<div id="carousel-example-generic" class="carousel slide" data-ride="carousel">  
-  <ol class="carousel-indicators">
-   <?php $pos = 0;
-    foreach ($data as $key=>$row):     
-              /* @var $row NewsModel */?>      
-     <li data-target="#carousel-example-generic" data-slide-to="<?=$key?>" class="<?=!($pos++)?"active":""?>"></li>    
-   <?php endforeach;?>    
-  </ol>
+<div class="carousel-atc wrapper">
+  <ol class="indicators"></ol>
+  
+  <ul class="img-part">
+    <?php foreach ($data as $key=>$row): ?>
+      <li class="item" data-area="<?=$key?>" img-src="<?=$row->icon?>">
+      </li>
+    <?php endforeach;?>
+  </ul>
 
-  <!-- Wrapper for slides -->
-  <div class="carousel-inner" role="listbox">
-   <?php  $pos = 0;
-          foreach ($data as $key=>$row):     
-              /* @var $row NewsModel */?>     
-    <div class="item <?=!($pos++)?"active":""?>">
-      <img src="<?=$row->icon?>" alt="">
-      <div class="carousel-caption">
-        <h4><?= $row->header?></h4>
-        <p><?= $row->text ?></p>        
-      </div>
-    </div>
-   <?php endforeach;?>
-  </div>
-
-  <!-- Controls -->
-  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
+  <a class="left arrow" href="#" role="button" data-arrow="prev">
     <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
     <span class="sr-only">Назад</span>
   </a>
-  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
+  <a class="right arrow" href="#" role="button" data-arrow="next">
     <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
     <span class="sr-only">Вперед</span>
   </a>
+
+  <div class="text-part">
+    <?php foreach ($data as $key=>$row): ?>
+      <div class="item" data-area="<?=$key?>">
+        <div class="header"><?= $row->header?></div>
+        <div class="text"><?= $row->text ?></div>
+      </div>
+    <?php endforeach;?>
+  </div>
 </div>
